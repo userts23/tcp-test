d053114485dca2a995f417be93bc2369d0ce1f4d
userts23 <140580513+userts23@users.noreply.github.com>
2023-08-02 09:12:56 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,38 @@
-// socket will close and destroy automatically after the device close the connection
-const net  = require("net")
-let port = 3000
-let host = "0.0.0.0"
-var s = net.createServer(function (sock) {
-    sock.setEncoding('utf8');
-    console.log("new connection", process.uptime())
+var http = require('http');
 
-    var body = "";
-    sock.on('data', function (data) {
-        body = body + data;
-    });
-    
+var LISTEN_ON_PORT = 3000;
 
-    sock.on('end', function() {
-        console.log(data);
-        postData(data);
+function toTitleCase(str) {
+    return str.replace(/[a-z]*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+}
+
+http.createServer(function (req, res) {
+    var body;
+
+    body = '';
+    req.on('data', function(chunk) {
+        body += chunk;
     });
 
-    // TODO error handling here
-});
+    req.on('end', function() {
+        console.log(req.method + ' ' + req.url + ' HTTP/' + req.httpVersion);
 
-s.listen(port, host, function () {
-    console.log('Server listening on ' + host + ':' + s.address().port);
+        for (prop in req.headers) {
+            console.log(toTitleCase(prop) + ': ' + req.headers[prop]);
+        }
+
+        if (body.length > 0) {
+            console.log('\n' + body);
+        }
+        console.log('');
+
+        res.writeHead(200);
+        res.end();
+    });
+
+    req.on('err', function(err) {
+        console.error(err);
+    });
+}).listen(LISTEN_ON_PORT, function () {
+    console.log('Server listening on port ' + LISTEN_ON_PORT);
 });
